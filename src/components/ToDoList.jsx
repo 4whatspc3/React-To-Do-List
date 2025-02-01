8eede1535b923e264d4641bee09276034ac96fe9
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-02-01 16:45:23 -0300

    fix: 'deleteTask' function
    Now it's possible to delete a task

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -19,7 +19,10 @@ function ToDoList() {
     }
   }
 
-  function deleteTask(index) {}
+  function deleteTask(index) {
+    const updatedTasks = task.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
 
   function moveTaskUp(index) {}
 
