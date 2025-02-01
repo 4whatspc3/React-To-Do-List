4f709534f8bb87bc403d7d66cd7b68161db671e5
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-02-01 14:47:27 -0300

    feat: Add functions and arrays to ToDoList component
    For now, the functions are empty and only serve to
    indicate which parts of the component need to be
    developed

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function ToDoList() {
+  const [task, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(e) {}
+
+  function addTask() {}
+
+  function deleteTask(index) {}
+
+  function moveTaskUp(index) {}
+
+  function moveTaskDown(index) {}
+
+  return <></>;
+}
+
+export default ToDoList;
